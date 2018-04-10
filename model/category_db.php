d5a0b3c0bc8fd406c0f15827b8820b11d39c08a4
saif khan <sk2437@afsconnect1.njit.edu>
2018-04-10 17:38:25 -0400

    week11

--- model/category_db.php
+++ model/category_db.php
@@ -20,4 +20,29 @@ function get_category_name($category_id) {
     $category_name = $category['categoryName'];
     return $category_name;
 }
+
+function add_category ($category_id, $category_name ) {
+    global $db;
+    $query = "INSERT INTO categories (categoryID, categoryName) VALUES (:category_id, :category_name )";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':category_id', $category_id);
+    $statement->bindValue(':category_name',$category_name );
+    $statement->execute();    
+    $category = $statement->fetch();
+    $statement->closeCursor();    
+   return $category_name;
+              
+}
+
+function delete_category ($category_id) {
+    global $db;
+    $query = "DELETE FROM categories WHERE categoryID = :category_id";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':category_id', $category_id);
+    $statement->execute();    
+  
+    $statement->closeCursor();    
+ 
+}
+
 ?>
